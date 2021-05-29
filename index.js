65e155b155b7d660ad22e7e964dd739e8b755cd4
sifat hassan <sifat007.sh7@gmail.com>
2021-05-29 15:00:45 +0600

    sent and get sms event

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 var http = require("http");
 const app = express();
@@ -5,33 +6,25 @@ const port = process.env.PORT || 5000;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 
-
 //middlewre
 app.use(express.json());
 var clients = {};
 
-
-io.on("connection",(socket) => {
-
-    console.log("conneted");
-    console.log(socket.id,"hass joined");
-
-    socket.on("signin", (id) => {
-        console.log(id);
-        clients[id] = socket;
-        console.log(clients);
-      });
-      
-      socket.on("message", (msg) => {
-        console.log(msg);
-        let targetId = msg.targetId;
-        if (clients[targetId]) clients[targetId].emit("message", msg);
-      });
-
-})
-
-
-
+io.on("connection", (socket) => {
+  console.log("connetetd");
+  console.log(socket.id, "has joined");
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
+  });
+  socket.on("message", (msg) => {
+    console.log(msg);
+    let targetId = msg.targetId;
+    if (clients[targetId]) clients[targetId].emit("message", msg);
+    console.log("sifat: "+clients);
+  });
+});
 server.listen(port, "0.0.0.0", () => {
     console.log("server started");
   });
