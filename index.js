39e3087856d1143038601fe7ac81c627b8cb9547
sifat hassan <sifat007.sh7@gmail.com>
2021-05-29 11:48:07 +0600

    install older version of socket io and test the connection with app

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require("express");
 var http = require("http");
 const app = express();
-const cors=require("cors")
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require("socket.io")(server,{
-    cors:
-    {
-        origin:"*"
-    }
-});
+var io = require("socket.io")(server);
 
 
 //middlewre
 app.use(express.json());
-app.use(cors());
 var clients = {};
 
 
 io.on("connection",(socket) => {
 
     console.log("conneted");
+    console.log(socket.id,"hass joined");
+    socket.on("/test",(msg)=>{
+        console.log(msg)
+    })
 
 })
 
